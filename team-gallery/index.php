4bfb24b52544febebb0eb9c034da8fc734fae7db
gogia145 <37367200+gogia145@users.noreply.github.com>
2018-03-16 16:39:58 +0530

    Circle Team gallery

--- team-gallery/index.php
+++ team-gallery/index.php
@@ -5,6 +5,7 @@
    Author: Vikas Gautam
 */
 
+
 function myteam_post() {
     $args = array(
       'public' => true,
@@ -67,7 +68,7 @@ function  team_list()
     	<!--Image1-->
 	<?php 
 	 global $post;
-	query_posts("post_type=myteam&showposts=-1"); 
+	query_posts("post_type=myteam&showposts=-1&orderby=rand"); 
 		     while(have_posts()):  the_post();
 			 
 			 $teamimage = get_the_post_thumbnail_url(get_the_ID(),'full');
@@ -77,59 +78,28 @@ function  team_list()
 			 //echo $post->ID;
 		?>
 		
+		<script>
+jQuery(document).ready(function(){
+    jQuery('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
        
         <div class='rp-item'>
             <div class='info-item round slide_effect to-top-left-corner'>
-                <a  id="clickbutton<?php echo $post->ID; ?>" ref="" target='' rel=''>
+                <a  id="clickbutton<?php echo $post->ID; ?>" ref="" target='' rel='' data-toggle="tooltip" data-placement="top" title="<?php echo get_post_meta($post->ID, 'our_views', true );  ?>">
                     <div class='img'>
                         <img src='<?php echo $teamimage; ?>' />
                     </div>
                     <div class='info'>
                         <h3><?php the_title(); ?></h3>
-                        <p><?php echo get_post_meta($post->ID, 'designation', true );  ?></p>
+                        <p><?php echo get_post_meta($post->ID, 'designation', true );  ?><br/></p>
                     </div>
                 </a>
             </div>
         </div>
-        <!-- The Modal -->
- 
-            <div id="modelpopup<?php echo $post->ID; ?>" class="modal">
-                <!-- Modal content -->
-                <div class="modal-content">
-                    <span class="close close<?php echo $post->ID; ?>">&times;</span>
-					   <h3 align="center">What Our Employees say about us!</h3>
-                        <h4 align="center"><?php echo get_post_meta($post->ID, 'our_views', true );  ?></h4>
-                </div>
-            </div>
-        
+      
 		
-		<script>
-	// Get the modal
-	var modal = document.getElementById('modelpopup<?php echo $post->ID; ?>');
-	
-	// Get the button that opens the modal
-	var btn = document.getElementById("clickbutton<?php echo $post->ID; ?>");
-	
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close<?php echo $post->ID; ?>")[0];
-	
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-		modal.style.display = "block";
-	}
-	
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
-	
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	}
-	</script>
+		
 	
 		<?php endwhile; wp_reset_query(); ?>
 		
